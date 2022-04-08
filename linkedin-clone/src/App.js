a2fd57cb03d3db2a4c3dc3db45ef9fda6096c92a
Oliwia <oliwia.lubiarz@widelab.co>
2022-04-08 18:22:34 +0200

    setup

---
+++ linkedin-clone/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+ return (
+  <div className="app">
+   {/* Header */}
+
+   {/* App Body */}
+    {/* Sidebar */}
+    {/* Feed */}
+    {/* Widgets */}
+  </div>
+ );
+}
+
+export default App;
