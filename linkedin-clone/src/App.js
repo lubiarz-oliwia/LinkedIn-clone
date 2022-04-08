602b02fe46b4e6095e57d6c7693291b2777e985e
Oliwia <oliwia.lubiarz@widelab.co>
2022-04-08 20:13:06 +0200

    Header

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
+import Header from "./Header";
 
 function App() {
  return (
   <div className="app">
    {/* Header */}
-
+   <Header />
    {/* App Body */}
-    {/* Sidebar */}
-    {/* Feed */}
-    {/* Widgets */}
+   {/* Sidebar */}
+   {/* Feed */}
+   {/* Widgets */}
   </div>
  );
 }
